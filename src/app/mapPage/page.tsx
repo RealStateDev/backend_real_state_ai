b7a8c77a85c66c64ec376f67964483b49c47a0ec
RealStateDev <tesis.realestateai@gmail.com>
2025-05-21 22:15:15 -0300

    Modified *Ver en Mapa* view

--- src/app/mapPage/page.tsx
+++ src/app/mapPage/page.tsx
@@ -1,16 +1,48 @@
-'use client'; // O asegúrate de usar esto en un Client Component
+"use client";
 
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
+import { FiArrowLeft } from "react-icons/fi";
+import { useRouter } from "next/navigation";
 
-const Mapa = dynamic(() => import('@/components/ui/commons/Mapa'), {
-  ssr: false, // Esto es lo importante
+const Mapa = dynamic(() => import("@/components/ui/commons/Mapa"), {
+  ssr: false,
 });
 
 export default function MapPage() {
+  const router = useRouter();
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Ver en mapa</h1>
-      <Mapa />
-    </div>
+    <>
+      {/* Boton para ir atras*/} 
+      <button
+        onClick={() => router.push("/savedPage")}
+        className={`
+          absolute
+          top-2 left-2            
+          sm:top-4 sm:left-4      
+          m-2                     
+          sm:m-4                  
+          p-1                     
+          sm:p-2                  
+          bg-white rounded-full shadow-md
+          hover:shadow-lg
+          transition
+          focus:outline-none focus:ring-2 focus:ring-blue-400
+        `}
+        aria-label="Volver a savedPage"
+      >
+        <FiArrowLeft className="w-5 h-5 sm:w-6 sm:h-6 text-blue-600" />
+      </button>
+      <div className=" relative pt-12 sm:pt-16 px-4 sm:px-6 max-w-full sm:max-w-5xl mx-auto">
+        <h1 className="text-2xl sm:text-3xl font-bold mb-2">Ver en mapa</h1>
+        <p className="text-sm sm:text-base text-gray-600 mb-6">
+          Aquí puedes ver la ubicación que seleccionaste. Usa los controles para
+          explorar el mapa.
+        </p>
+
+        <div className="w-full h-[50vh] sm:h-[60vh] md:h-[70vh] lg:h-[80vh]">
+          <Mapa />
+        </div>
+      </div>
+    </>
   );
 }
